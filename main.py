e6e49eff2edccb867fa553506b89c74f97ead13c
jackmckenzie98 <jack.mckenzie98@outlook.com>
2023-08-28 16:09:48 -0500

    Refactored code to format existing objects and compare correctly

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import json
 import os
 import boto3
 
-migrate_from = "https://debian-pingfed:9999/pf-admin-api/v1"
+migrate_from = os.environ.get('MIGRATE_FROM')
 
 #Pull secret from secrets manager
 def get_secret(secret_name):
@@ -29,22 +29,23 @@ with open("apiCalls.json") as f:
     endpoint = json.load(f)
 
 session = requests.Session()
-secrets = get_secret('debian-pf-api-secret')
+secrets = get_secret(os.environ.get('API_SECRET'))
 session.auth = (secrets["username"], secrets["pass"])
 session.headers.update({'X-XSRF-Header': 'PingFederate'})
 session.headers.update({'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br'})
 session.verify = False
 #Create a directory for the artifacts in the current working directory
-file_path = os.getcwd()
-final_path = os.path.join(file_path, r'artifactsPull')
-cert_path = os.path.join(final_path, r'certs')
-if not os.path.exists(final_path):
-    os.makedirs(os.path.join(file_path, r'artifactsPull'))
+WORKING_DIRECTORY = os.getcwd()
+ARTIFACTS_PATH = os.path.join(WORKING_DIRECTORY, r'artifactsPull')
+CERTIFICATES_PATH = os.path.join(ARTIFACTS_PATH, r'certs')
+if not os.path.exists(ARTIFACTS_PATH):
+    os.makedirs(os.path.join(WORKING_DIRECTORY, r'artifactsPull'))
 
-if not os.path.exists(cert_path):
-    os.makedirs(os.path.join(final_path, f'certs'))
+if not os.path.exists(CERTIFICATES_PATH):
+    os.makedirs(os.path.join(ARTIFACTS_PATH, f'certs'))
+
 def make_calls():
-    get_secret('debian-pf-api-secret')
+    get_secret(os.environ.get('API_SECRET'))
     clients = session.get(migrate_from + endpoint["oauthClients"]["endpoint"]).json()
     spConnections = session.get(migrate_from + endpoint["spConnections"]["endpoint"]).json()
     authPolicies = session.get(migrate_from + endpoint["authPolicies"]["endpoint"]).json()
@@ -60,17 +61,37 @@ def make_calls():
     return [clients, spConnections, authPolicies, authenticationPolicyFragments, idpAdapters, passwordCVs, accessTokenManagers, accessTokenMappings,\
         authPolicyContracts, dataStores, keyPairs]
 
-def write_to_file():
-    list_thing = make_calls()
-    file_names = ["clients", "spConnections", "authPolicies", "authenticationPolicyFragments", "idpAdapters", "passwordCredentialValidators",
-                  "accessTokenManagers", "accessTokenMappings", "authPolicyContracts", "dataStores", "keyPairs"]
-    for name, obj in zip(file_names, list_thing):
-        f = open(f"{final_path}/{name}.json", 'w+')
+
+#Format any JSON objects that are not formatted in the same way as the others, with a JSON object that wraps it in "items",
+#like the other objects do.
+def format_object(list_deal):
+    for i in range(0, len(list_deal)):
+        #Check if the type is a list, because if so it should be wrapped in "items" key
+        if type(list_deal[i]) is list:
+            list_deal[i] = {
+                "items" : list_deal[i]
+            }
+        #Check if type is a dictionary but does not have "items" key that matches other file formats
+        if type(list_deal[i]) is dict:
+            if 'items' not in list_deal[i]:
+                list_deal[i] = {
+                    "items": [list_deal[i]]
+                }
+    return list_deal
+
+def write_to_file(list_passed):
+    file_names = ["clients", "spConnections", "authPolicies", "authenticationPolicyFragments", "idpAdapters",
+                  "passwordCredentialValidators", "accessTokenManagers", "accessTokenMappings", "authPolicyContracts",
+                  "dataStores", "keyPairs"]
+    for name, obj in zip(file_names, list_passed):
+        f = open(f"{ARTIFACTS_PATH}/{name}.json", 'w+')
         f.write(json.dumps(obj, indent=3))
         f.close()
 
 
-write_to_file()
+list_passed = make_calls()
+formatted_list = format_object(list_passed)
+write_to_file(formatted_list)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
